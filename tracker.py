2e86e94ae69d37c3d807f32cd0f748fc63d2ac32
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-08 16:27:50 +0100

    added get_crypto_data() function

--- tracker.py
+++ tracker.py
@@ -1,4 +1,16 @@
-from urllib import Request, urlopen
-import gzip
+import requests
 import json
+
+# CoinCap API URL
+URL = 'http://api.coincap.io/v2/assets'
+
+# get the current crypto data from CoinCap API
+def get_crypto_data():
+    
+    # make a GET request to retrieve all current cryptocurrency data
+    response = requests.get(URL)
+    
+    # parse the data into json format and return the data portion
+    parsed_data = json.loads(response.text)
+    return parsed_data['data']
 
