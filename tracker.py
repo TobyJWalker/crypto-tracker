0ff6f3cfa20bf5d971044eb21d970d8570d79ba7
TobyJWalker <tobyjwalker03@outlook.com>
2023-09-08 16:32:39 +0100

    added reference json layout

--- tracker.py
+++ tracker.py
@@ -4,6 +4,22 @@ import json
 # CoinCap API URL
 URL = 'http://api.coincap.io/v2/assets'
 
+''' Example layout
+{
+      "id": "bitcoin",
+      "rank": "1",
+      "symbol": "BTC",
+      "name": "Bitcoin",
+      "supply": "17193925.0000000000000000",
+      "maxSupply": "21000000.0000000000000000",
+      "marketCapUsd": "119150835874.4699281625807300",
+      "volumeUsd24Hr": "2927959461.1750323310959460",
+      "priceUsd": "6929.8217756835584756",
+      "changePercent24Hr": "-0.8101417214350335",
+      "vwap24Hr": "7175.0663247679233209"
+    }
+'''
+
 # get the current crypto data from CoinCap API
 def get_crypto_data():
     
@@ -14,3 +30,4 @@ def get_crypto_data():
     parsed_data = json.loads(response.text)
     return parsed_data['data']
 
+def get_data_by_symbol(symbol)
